39449008be618b9fa8d5fd4a52d0932f62990e48
Olga Gorobets <dorofeeva.olga74@mail.ru>
2023-11-10 00:11:26 +0900

    B39

--- controllers/cards.js
+++ controllers/cards.js
@@ -47,11 +47,11 @@ module.exports.deleteCard = async (req, res) => {
       }
     })
     .catch((err) => {
-      if (err.name === "ValidationError") {
+      //if (err.name === "ValidationError") {
         res.status(400).send({ message: "Ошибка валидации полей", ...err });
-       } else {
-        res.status(404).send({ message: "Карточка не найдена" });
-      }
+      // } else {
+      //  res.status(404).send({ message: "Карточка не найдена" });
+      //}
       //res.status(500).send({ message: "На сервере произошла ошибка" })
     });
 };
