07d0c8708d63981c84587769d78b27fa768694b4
Olga Gorobets <dorofeeva.olga74@mail.ru>
2023-11-10 23:25:13 +0900

    B49

--- errors/BadRequest.js
+++ errors/BadRequest.js
@@ -1,8 +1,8 @@
-const { HTTP_STATUS_BAD_REQUEST } = require("http2").constants;
+//const { HTTP_STATUS_BAD_REQUEST } = require("http2").constants;
 
-module.exports = class BadRequestError extends Error {
+module.exports = class BadRequest extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = HTTP_STATUS_BAD_REQUEST;//400;
+    this.statusCode = 400;
   }
 }
